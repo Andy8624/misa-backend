a0a603b7f82b06d2500d138dd0698188556a593b
Duong Lap Khang <khangss2803@gmail.com>
2025-06-11 10:22:25 +0700

    refactor: change jwt expire time

--- src/config/jwt/jwt.config.ts
+++ src/config/jwt/jwt.config.ts
@@ -1,7 +1,7 @@
 export const jwtConfig = {
   accessToken: {
     secret: process.env.ACCESS_TOKEN_SECRET,
-    expiresIn: '999d',
+    expiresIn: '1d',
   },
   refreshToken: {
     secret: process.env.REFRESH_TOKEN_SECRET,
